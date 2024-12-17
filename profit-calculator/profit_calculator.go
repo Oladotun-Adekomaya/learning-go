696ae25225a724b1f55bb3bac70a2926cf785b57
oladotun <oladotunadekomaya@gmail.com>
2024-12-17 23:32:48 +0100

    commit

--- profit-calculator/profit_calculator.go
+++ profit-calculator/profit_calculator.go
@@ -27,10 +27,14 @@ func main(){
 	profit := ebt - (ebt*(taxRate/100))
 	ratio := ebt / profit
 
-	fmt.Printf("The earnings before tax and interest is: %v\n", ebt)
+	// fmt.Printf("The earnings before tax and interest is: %v\n", ebt)
+	// fmt.Printf("The profit is %v\n",profit)
+	// fmt.Printf("The ratio is %.2f\n",ratio)
 
-	// fmt.Println(ebt)
-	fmt.Println(profit)
-	fmt.Printf("The ratio is %.2f\n",ratio)
+	ratiotxt := fmt.Sprintf("The earnings to profit ratio is %.2f\n",ratio)
+	profittxt := fmt.Sprintf("The profit is %v\n",profit)
+	earningstxt := fmt.Sprintf("The earnings before tax and interest is: %v\n", ebt)
+
+	fmt.Print(earningstxt, profittxt, ratiotxt)
 	
 }
