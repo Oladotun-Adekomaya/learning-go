9a9ea5cfa777f01b5a00f37d3e60ea4f2127cb40
oladotun <oladotunadekomaya@gmail.com>
2024-12-17 17:20:37 +0100

    commit

--- profit-calculator/profit_calculator.go
+++ profit-calculator/profit_calculator.go
@@ -7,5 +7,22 @@ import (
 )
 
 func main(){
-	fmt.Println("Hello world")
+	var revenue, expenses, taxRate float64
+	
+	fmt.Println("Revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Println("Expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Println("Tax Rate: ")
+	fmt.Scan(&taxRate)
+
+	ebt := revenue - expenses
+	profit := ebt - (ebt*(taxRate/100))
+	ratio := ebt/profit
+
+	println("Ebt: ", ebt)
+	println("Expenses: ", expenses)
+	println("Ratio: ", ratio)
 }
