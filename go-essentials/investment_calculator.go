3bff184a31aee3332ff9d6c8a925308fbe6f2af1
oladotun <oladotunadekomaya@gmail.com>
2024-12-17 23:58:27 +0100

    commit

--- go-essentials/investment_calculator.go
+++ go-essentials/investment_calculator.go
@@ -18,14 +18,16 @@ func main()  {
 	fmt.Print("Expected return rate: ")
 	fmt.Scan(&expectedReturnRate)
 
-	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100,years)
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+	futureValue, futureRealValue := calculateFutureValues(investmentAmount, expectedReturnRate, years)
+
+	// futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100,years)
+	// futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println(futureValue, futureRealValue)
 }
 
-func calculateFutureValues(){
+func calculateFutureValues(investmentAmount, expectedReturnRate, years float64) (float64, float64){
 	fv := investmentAmount * math.Pow(1+expectedReturnRate/100,years)
-	rfv := futureValue / math.Pow(1+inflationRate/100, years)
+	rfv := fv / math.Pow(1+inflationRate/100, years)
 	return fv, rfv
 }
