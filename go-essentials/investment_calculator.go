468a7fa7543d28bfb86092327327f66e4725a30e
oladotun <oladotunadekomaya@gmail.com>
2024-12-17 23:53:37 +0100

    commit

--- go-essentials/investment_calculator.go
+++ go-essentials/investment_calculator.go
@@ -4,9 +4,9 @@ import (
 	"fmt"
 	"math"
 )
-
+const inflationRate = 2.5
 func main()  {
-	const inflationRate = 2.5
+	
 	var investmentAmount, years, expectedReturnRate float64   
 
 	fmt.Print("Investment amount: ")
@@ -22,4 +22,10 @@ func main()  {
 	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println(futureValue, futureRealValue)
+}
+
+func calculateFutureValues(){
+	fv := investmentAmount * math.Pow(1+expectedReturnRate/100,years)
+	rfv := futureValue / math.Pow(1+inflationRate/100, years)
+	return fv, rfv
 }
