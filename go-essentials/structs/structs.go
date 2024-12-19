33ddedd3526f88d8d39578d7d61a0e5b31205e69
oladotun <oladotunadekomaya@gmail.com>
2024-12-19 17:53:51 +0100

    commit

--- go-essentials/structs/structs.go
+++ go-essentials/structs/structs.go
@@ -16,6 +16,11 @@ func (u user) outputUserDetails() {
 	fmt.Println(u.firstName, u.lastName, u.birthDate)
 }
 
+func (u *user) clearUserName() {
+	u.firstName = ""
+	u.lastName = ""
+}
+
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
@@ -38,6 +43,8 @@ func main() {
 
 	// ... do something awesome with that gathered data!
 
+	appUser.outputUserDetails()
+	appUser.clearUserName()
 	appUser.outputUserDetails()
 }
 
