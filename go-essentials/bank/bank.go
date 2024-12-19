4a0c2f2ea44940bd647fb34cad1c636a314ea80e
oladotun <oladotunadekomaya@gmail.com>
2024-12-19 13:32:55 +0100

    commit

--- go-essentials/bank/bank.go
+++ go-essentials/bank/bank.go
@@ -86,11 +86,3 @@ func main() {
 	fmt.Println("This the end the code")
 	// fmt.Println("Your choice:",choice)
 }
-
-func presentOptions() {
-	fmt.Println("What do you want to do?")
-	fmt.Println("1. Check balance")
-	fmt.Println("2. Deposit money")
-	fmt.Println("3. Withdraw money")
-	fmt.Println("4. Exit")
-}
