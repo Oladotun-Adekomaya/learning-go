31a43adf297d7db02f7fe35e018ac75c7d8b2f38
oladotun <oladotunadekomaya@gmail.com>
2024-12-19 17:33:11 +0100

    commit

--- go-essentials/structs/structs.go
+++ go-essentials/structs/structs.go
@@ -12,32 +12,33 @@ type user struct {
 	createdAt time.Time
 }
 
+func (u user) outputUserDetails() {
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
+}
+
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ")
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
 
-	var appUser user = user{
-		firstName: userFirstName,
-		lastName:  userLastName,
-		birthDate: userBirthdate,
-		createdAt: time.Now(),
-	}
-
-	// appUser = user{
+	// var appUser user = user{
 	// 	firstName: userFirstName,
 	// 	lastName:  userLastName,
 	// 	birthDate: userBirthdate,
 	// 	createdAt: time.Now(),
 	// }
 
+	var appUser user
+	appUser = user{
+		firstName: userFirstName,
+		lastName:  userLastName,
+		birthDate: userBirthdate,
+		createdAt: time.Now(),
+	}
+
 	// ... do something awesome with that gathered data!
 
 	outputUserDetails(appUser)
-}
-
-func outputUserDetails(u user) {
-	fmt.Println(u.firstName, u.lastName, u.birthDate)
 }
 
 func getUserData(promptText string) string {
