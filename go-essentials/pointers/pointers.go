d6d492aa2bca9c50708b0c0e97f069f7c59d15ef
oladotun <oladotunadekomaya@gmail.com>
2024-12-19 18:19:35 +0100

    commit

--- go-essentials/pointers/pointers.go
+++ go-essentials/pointers/pointers.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	age := 32 //regular variable
+
+	agePointer := &age
+
+	fmt.Println("Age:", age)
+
+	adultYears := getAdultYears(age)
+	fmt.Println(adultYears)
+}
+
+func getAdultYears(age int) int {
+	return age - 18
 }
