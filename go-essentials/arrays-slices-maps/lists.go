5ab51f2e577341bd411df18422493a4dc149223e
oladotun <oladotunadekomaya@gmail.com>
2024-12-22 23:28:24 +0100

    commit

--- go-essentials/arrays-slices-maps/lists.go
+++ go-essentials/arrays-slices-maps/lists.go
@@ -5,10 +5,10 @@ import (
 )
 
 func main() {
-	// prices := [4]float64{10.99, 30.0, 19.5, 50.0}
+	prices := [4]float64{10.99, 30.0, 19.5, 50.0}
 
-	var productNames [3]string
-	productNames = [3]string{""}
+	var productNames [3]string = [3]string{"A Book", "A Pen"}
 
 	fmt.Println(productNames)
+	fmt.Println(prices[0])
 }
